6d27f88b2cdde643976954e6978c14573596c524
Jackie D'Elia <jackie@jackiedelia.com>
2018-12-13 09:14:53 -0500

    Add scroll with jQuery.

--- js/app.js
+++ js/app.js
@@ -1,10 +1,19 @@
-"use strict";
-// Run SmoothScroll once page has loaded.
-// window.onload = function () {
-// 	var scroll = new SmoothScroll('a[href*="#"]', {
-// 		speed: 800,
-// 		speedAsDuration: true
-// 	});
-// }
-
+$(function () {
+	$('a[href*=#]:not([href=#])').click(function (event) {
+		if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+			target = $(this.hash);
+			target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+			if (target.length) {
+				$('html,body').animate({
+					scrollTop: target.offset().top
+				}, 875, function () {
+					target.attr('tabindex', '-1');
+					target.focus();
+				});
+				location.hash = target;
+				return false;
+			}
+		}
+	});
+});
 
