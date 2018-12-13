292542511f02dbdda58fc4e9237abdb9e3de618f
Jackie D'Elia <jackie@jackiedelia.com>
2018-12-13 07:32:29 -0500

    Clean up styles. Comment out js.

--- js/app.js
+++ js/app.js
@@ -1,8 +1,10 @@
 "use strict";
 // Run SmoothScroll once page has loaded.
-window.onload = function () {
-	var scroll = new SmoothScroll('a[href*="#"]', {
-		speed: 800,
-		speedAsDuration: true
-	});
-}
+// window.onload = function () {
+// 	var scroll = new SmoothScroll('a[href*="#"]', {
+// 		speed: 800,
+// 		speedAsDuration: true
+// 	});
+// }
+
+
